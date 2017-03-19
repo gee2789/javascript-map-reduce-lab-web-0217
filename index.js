8b52e2427ab6b722bcc6641302b1b2636182a0cb
gee2789 <geeyung.li@gmail.com>
2017-03-19 18:33:53 -0400

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,47 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+var issuesWithUpdatedApiUrl = issues.map(function(issue){
+  return Object.assign({}, issue, {
+    url: issue.url.replace('api.github.com', 'api-v2.github.com')
+  })
+});
+// const issuesWithUpdatedApiUrl = issues
+//   .map(issue => Object.assign({}, issue, {
+//     url: issue.url.replace('api.github.com', 'api-v2.github.com')
+//   }));
+
+// var commentCountAcrossIssues = issues.map(function(issue){
+//   issues.comments_count.reduce((allcount, count)=>{
+//     allcount + count;
+//   }, 0);
+// })
+
+const commentCountAcrossIssues = issues
+  .map(issue => issue.comments_count)
+  .reduce((total, count) => total + count, 0);
+
+  const openIssues = issues.reduce((openIssues, issue) => {
+    if (issue.state === 'open') {
+      return [...openIssues, issue];
+    }
+    return openIssues;
+  }, []);
+
+  const nonAutomaticIssues = issues.reduce((nonAutomatic, issue) => {
+    if (!issue.body.includes('automatically created by learn.co.')){
+      return [...nonAutomatic, issue];
+    }
+    return nonAutomatic;
+  }, []);
+
+  const $tbody = document.getElementById('results');
+$tbody.innerHTML = nonAutomaticIssues
+  .map(issue => `<tr>
+    <td>${issue.body}</td>
+    <td>${issue.created_at}</td>
+    <td>${issue.state}</td>
+    </tr>`
+  )
+  .join('');
